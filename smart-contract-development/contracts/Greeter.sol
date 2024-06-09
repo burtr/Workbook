5f3bdfde17e58b3d3a2df07a1945d26a99afb3c1
Burt Rosenberg <burt@cs.miami.edu>
2024-06-09 14:30:20 -0400

    committd

--- smart-contract-development/contracts/Greeter.sol
+++ smart-contract-development/contracts/Greeter.sol
@@ -2,8 +2,30 @@ pragma solidity >= 0.4.0 < 0.9.0 ;
 
 contract Greeter {
 
-	function greet() external pure returns(string memory) {
-		return "Hello World!" ;
+	string private _greeting = "Hello World!" ;
+	address private _owner ;
+
+	constructor () public {
+		_owner = msg.sender ;
+	}
+
+	modifier onlyOwner() {
+		require (
+			msg.sender == _owner, "Ownable: caller is not the owner"
+		) ;
+		_ ;
+	}
+
+	function greet() external view returns(string memory) {
+		return _greeting ;
+	}
+
+	function setGreeting(string calldata greeting) external onlyOwner {
+		_greeting = greeting ;
+	} 
+
+	function owner() public view returns(address) {
+		return _owner ;
 	}
 
 }
